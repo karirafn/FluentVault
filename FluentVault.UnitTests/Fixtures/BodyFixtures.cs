27a506101e9a42c75a14dbfc601d5acc68c6a756
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-16 11:08:24 +0000

    Added test for all vault files parser and moved test fixtures to seperate class

---
+++ FluentVault.UnitTests/Fixtures/BodyFixtures.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+using AutoFixture;
+
+namespace FluentVault.UnitTests.Fixtures;
+
+internal static class BodyFixtures
+{
+    public static (string Body, IEnumerable<VaultFile> Files) GetVaultFileFixtures(int count)
+    {
+        Fixture fixture = new();
+        List<VaultFile> files = new();
+        StringBuilder bodybuilder = new();
+
+        bodybuilder.Append($@"
+<s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"">
+    <s:Body xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+        <FindFilesBySearchConditionsResponse xmlns=""http://AutodeskDM/Services/Document/1/7/2020/"">
+            <FindFilesBySearchConditionsResult>");
+
+        for (int i = 0; i < count; i++)
+        {
+            VaultFile file = fixture.Create<VaultFile>();
+            files.Add(file);
+            bodybuilder.Append($@"<File
+                    Id=""{file.Id}""
+                    Name=""{file.Filename}""
+                    VerName=""{file.VersionName}""
+                    MasterId=""{file.MasterId}""
+                    VerNum=""{file.VersionNumber}""
+                    MaxCkInVerNum=""{file.MaximumCheckInVersionNumber}""
+                    CkInDate=""{file.ChecedkInDate}""
+                    Comm=""{file.Comment}""
+                    CreateDate=""{file.CreatedDate}""
+                    CreateUserId=""{file.CreateUserId}""
+                    Cksum=""{file.CheckSum}""
+                    FileSize=""{file.FileSize}""
+                    ModDate=""{file.ModifiedDate}""
+                    CreateUserName=""{file.CreateUserName}""
+                    CheckedOut=""{file.IsCheckedOut}""
+                    FolderId=""{file.FolderId}""
+                    CkOutSpec=""{file.CheckedOutPath}""
+                    CkOutMach=""{file.CheckedOutMachine}""
+                    CkOutUserId=""{file.CheckedOutUserId}""
+                    FileClass=""{file.FileClass}""
+                    Locked=""{file.IsLocked}""
+                    Hidden=""{file.IsHidden}""
+                    Cloaked=""{file.IsCloaked}""
+                    FileStatus=""{file.FileStatus}""
+                    IsOnSite=""{file.IsOnSite}""
+                    DesignVisAttmtStatus=""{file.DesignVisualAttachmentStatus}""
+                    ControlledByChangeOrder=""{file.IsControlledByChangeOrder}"">                                                          
+                    <FileRev
+                        RevId=""{file.Revision?.Id}""
+                        Label=""{file.Revision?.Label}""
+                        MaxConsumeFileId=""{file.Revision?.MaximumConsumeFileId}""
+                        MaxFileId=""{file.Revision?.MaximumFileId}""
+                        RevDefId=""{file.Revision?.DefinitionId}""
+                        MaxRevId=""{file.Revision?.MaximumRevisionId}""
+                        Order=""{file.Revision?.Order}""/>
+                    <FileLfCyc
+                        LfCycStateId=""{file.Lifecycle?.StateId}""
+                        LfCycDefId=""{file.Lifecycle?.DefinitionId}""
+                        LfCycStateName=""{file.Lifecycle?.StateName}""
+                        Consume=""{file.Lifecycle?.IsConsume}""
+                        Obsolete=""{file.Lifecycle?.IsObsolete}""/>
+                    <Cat
+                        CatId=""{file.Category?.Id}""
+                        CatName=""{file.Category?.Name}""/>
+                </File>");
+        }
+
+        bodybuilder.Append(@"</FindFilesBySearchConditionsResult>
+            <bookmark/>
+            <searchstatus TotalHits=""1"" IndxStatus=""IndexingComplete""/>
+        </FindFilesBySearchConditionsResponse>
+    </s:Body>
+</s:Envelope>");
+
+        return (bodybuilder.ToString(), files);
+    }
+}
--- FluentVault.UnitTests/Systems/ExtensionTests.cs
+++ FluentVault.UnitTests/Systems/ExtensionTests.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 
 using AutoFixture;
 
 using FluentAssertions;
 using FluentAssertions.Extensions;
+
+using FluentVault.UnitTests.Fixtures;
 
 using Xunit;
 
@@ -17,15 +20,15 @@ public class ExtensionTests
     public void GetElementValue_ShouldReturnValue_WhenDocumentContainsElement()
     {
         // Arrange
-        var expected = "someValue";
+        var expectation = "someValue";
         var elementName = "SomeElement";
-        var document = XDocument.Parse($"<{elementName}>{expected}</{elementName}>");
+        var document = XDocument.Parse($"<{elementName}>{expectation}</{elementName}>");
 
         // Act
         var result = document.GetElementValue(elementName);
 
         // Assert
-        result.Should().Be(expected);
+        result.Should().Be(expectation);
     }
 
     [Fact]
@@ -45,89 +48,49 @@ public class ExtensionTests
     public void GetElementValues_ShouldReturnValues_WhenDocumentContainsElements()
     {
         // Arrange
-        var expected = ("someValue", "someOtherValue");
+        var expectation = ("someValue", "someOtherValue");
         var firstElement = "FirstElement";
         var secondElement = "SecondElement";
-        var document = XDocument.Parse($"<Root><{firstElement}>{expected.Item1}</{firstElement}><{secondElement}>{expected.Item2}</{secondElement}></Root>");
+        var document = XDocument.Parse($"<Root><{firstElement}>{expectation.Item1}</{firstElement}><{secondElement}>{expectation.Item2}</{secondElement}></Root>");
 
         // Act
         var result = document.GetElementValues(firstElement, secondElement);
 
         // Assert
-        result.Should().Be(expected);
+        result.Should().Be(expectation);
     }
 
     [Fact]
     public void ParseVaultFile_ShouldReturnFile_WhenParsingValidString()
     {
         // Arrange
-        Fixture fixture = new();
-        VaultFile expectedFile = fixture.Create<VaultFile>();
-
-        var text = $@"
-<s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"">
-    <s:Body xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-        <FindFilesBySearchConditionsResponse xmlns=""http://AutodeskDM/Services/Document/1/7/2020/"">
-            <FindFilesBySearchConditionsResult>     
-                <File
-                    Id=""{expectedFile.Id}""
-                    Name=""{expectedFile.Filename}""
-                    VerName=""{expectedFile.VersionName}""
-                    MasterId=""{expectedFile.MasterId}""
-                    VerNum=""{expectedFile.VersionNumber}""
-                    MaxCkInVerNum=""{expectedFile.MaximumCheckInVersionNumber}""
-                    CkInDate=""{expectedFile.ChecedkInDate}""
-                    Comm=""{expectedFile.Comment}""
-                    CreateDate=""{expectedFile.CreatedDate}""
-                    CreateUserId=""{expectedFile.CreateUserId}""
-                    Cksum=""{expectedFile.CheckSum}""
-                    FileSize=""{expectedFile.FileSize}""
-                    ModDate=""{expectedFile.ModifiedDate}""
-                    CreateUserName=""{expectedFile.CreateUserName}""
-                    CheckedOut=""{expectedFile.IsCheckedOut}""
-                    FolderId=""{expectedFile.FolderId}""
-                    CkOutSpec=""{expectedFile.CheckedOutPath}""
-                    CkOutMach=""{expectedFile.CheckedOutMachine}""
-                    CkOutUserId=""{expectedFile.CheckedOutUserId}""
-                    FileClass=""{expectedFile.FileClass}""
-                    Locked=""{expectedFile.IsLocked}""
-                    Hidden=""{expectedFile.IsHidden}""
-                    Cloaked=""{expectedFile.IsCloaked}""
-                    FileStatus=""{expectedFile.FileStatus}""
-                    IsOnSite=""{expectedFile.IsOnSite}""
-                    DesignVisAttmtStatus=""{expectedFile.DesignVisualAttachmentStatus}""
-                    ControlledByChangeOrder=""{expectedFile.IsControlledByChangeOrder}"">                                                          
-                    <FileRev
-                        RevId=""{expectedFile.Revision?.Id}""
-                        Label=""{expectedFile.Revision?.Label}""
-                        MaxConsumeFileId=""{expectedFile.Revision?.MaximumConsumeFileId}""
-                        MaxFileId=""{expectedFile.Revision?.MaximumFileId}""
-                        RevDefId=""{expectedFile.Revision?.DefinitionId}""
-                        MaxRevId=""{expectedFile.Revision?.MaximumRevisionId}""
-                        Order=""{expectedFile.Revision?.Order}""/>
-                    <FileLfCyc
-                        LfCycStateId=""{expectedFile.Lifecycle?.StateId}""
-                        LfCycDefId=""{expectedFile.Lifecycle?.DefinitionId}""
-                        LfCycStateName=""{expectedFile.Lifecycle?.StateName}""
-                        Consume=""{expectedFile.Lifecycle?.IsConsume}""
-                        Obsolete=""{expectedFile.Lifecycle?.IsObsolete}""/>
-                    <Cat
-                        CatId=""{expectedFile.Category?.Id}""
-                        CatName=""{expectedFile.Category?.Name}""/>
-                </File>
-            </FindFilesBySearchConditionsResult>
-            <bookmark/>
-            <searchstatus TotalHits=""1"" IndxStatus=""IndexingComplete""/>
-        </FindFilesBySearchConditionsResponse>
-    </s:Body>
-</s:Envelope>";
-        var document = XDocument.Parse(text);
+        var (body, expectation) = BodyFixtures.GetVaultFileFixtures(1);
+        var document = XDocument.Parse(body);
 
         // Act
-        var file = document.ParseVaultFile();
+        var result = document.ParseVaultFile();
 
         // Assert
-        file.Should().BeEquivalentTo(expectedFile, options => options
+        result.Should().BeEquivalentTo(expectation.Single(), options => options
+                .Using<DateTime>(ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, 1.Seconds()))
+                .WhenTypeIs<DateTime>());
+    }
+
+    [Fact]
+    public void ParseAllVaultFiles_ShouldReturnAllFiles_WhenParsingValidString()
+    {
+        // Arrange
+        Fixture fixture = new();
+        int fileCount = 3;
+        var (body, expectation) = BodyFixtures.GetVaultFileFixtures(fileCount);
+        var document = XDocument.Parse(body);
+
+        // Act
+        var result = document.ParseAllVaultFiles();
+
+        // Assert
+        result.Should().HaveCount(fileCount);
+        result.Should().BeEquivalentTo(expectation, options => options
                 .Using<DateTime>(ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, 1.Seconds()))
                 .WhenTypeIs<DateTime>());
     }
