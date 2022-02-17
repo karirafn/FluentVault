9961f7166c5d898e6860da730bf73c763f707e9d
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-17 14:37:29 +0000

    Added a test for lifecycle parsing

---
+++ FluentVault.UnitTests/Systems/VaultLifecycleParsingExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+
+using FluentAssertions;
+
+using FluentVault.UnitTests.Fixtures;
+
+using Xunit;
+
+namespace FluentVault.UnitTests.Systems;
+
+public class VaultLifecycleParsingExtensionsTests
+{
+    [Fact]
+    public void ParseVaultLifecycle_ShouldReturnValidResult_WhenParsingValidString()
+    {
+        // Arrange
+        var (body, expectation) = VaultResponseFixtures.GetVaultLifecycleFixtures(5);
+        var document = XDocument.Parse(body);
+
+        // Act
+        var result = document.ParseLifecycles();
+
+        // Assert
+        result.Should().BeEquivalentTo(expectation);
+    }
+}
