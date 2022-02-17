04924738b7f6d27fc3691ec14fd0a2596aa5c7b6
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-17 13:56:18 +0000

    Removed individual parsing methods as they are now generic

--- FluentVault/ExtensionMethods/GeneralXDocumentExtensions.cs
+++ FluentVault/ExtensionMethods/GeneralXDocumentExtensions.cs
@@ -12,21 +12,7 @@ internal static class GeneralXDocumentExtensions
         => element.Attribute(name)?.Value
         ?? throw new KeyNotFoundException($@"Attribute ""{name}"" was not found in element ""{element.Name}"".");
 
-    internal static long ParseAttributeAsLong(this XElement element, string name)
-        => long.TryParse(element.GetAttributeValue(name), out long value)
-        ? value
-        : throw new ArgumentException($@"Failed to parse attribute ""{name}"" as type long");
-
-    internal static bool ParseAttributeAsBool(this XElement element, string name)
-        => bool.TryParse(element.GetAttributeValue(name), out bool value)
-        ? value
-        : throw new ArgumentException($@"Failed to parse attribute ""{name}"" as type bool");
-
-    internal static DateTime ParseAttributeAsDateTime(this XElement element, string name)
-        => DateTime.TryParse(element.GetAttributeValue(name), out DateTime value)
-        ? value
-        : throw new ArgumentException($@"Failed to parse attribute ""{name}"" as type DateTime");
-    internal static T ParseAttributeValueAsType<T>(this XElement element, string name, Func<string, T> parse)
+    internal static T ParseAttribute<T>(this XElement element, string name, Func<string, T> parse)
         => parse(element.GetAttributeValue(name));
 
     internal static T ParseSingleElement<T>(this XContainer container, string name, Func<XElement, T> parse)
