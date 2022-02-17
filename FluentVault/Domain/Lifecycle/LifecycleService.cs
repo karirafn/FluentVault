c0b8bbe47e69496eeac21b30e93242f5008bc1a0
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-17 16:40:57 +0000

    Reorganized

---
+++ FluentVault/Domain/Lifecycle/LifecycleService.cs
@@ -0,0 +1,31 @@
+﻿namespace FluentVault;
+
+internal class LifecycleService
+{
+    private readonly VaultSessionInfo _session;
+
+    public LifecycleService(VaultSessionInfo session)
+    {
+        _session = session;
+    }
+
+    public async Task<IEnumerable<VaultLifecycle>> GetLifecycles()
+    {
+        var body = GetRequestBody();
+        Uri uri = new($"http://{_session.Server}/AutodeskDM/Services/v26/LifeCycleService.svc?op=GetAllLifeCycleDefinitions&uid=8&currentCommand=Connectivity.Explorer.Admin.AdminToolsCommand&vaultName={_session.Database}&app=VP");
+        var soapAction = @"""http://AutodeskDM/Filestore/Auth/1/8/2021/AuthService/SignOut""";
+
+        var document = await VaultHttpClient.SendRequestAsync(uri, body, soapAction);
+        var lifecycles = document.ParseLifecycles();
+
+        return lifecycles;
+    }
+
+    private string GetRequestBody()
+    {
+        var innerBody = @"		<GetAllLifeCycleDefinitions xmlns=""http://AutodeskDM/Services/LifeCycle/1/7/2020/""/>";
+        var requestBody = BodyBuilder.GetRequestBody(innerBody, _session.Ticket, _session.UserId);
+
+        return requestBody;
+    }
+}
