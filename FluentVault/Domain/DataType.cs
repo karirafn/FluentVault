d8659be0815dfe30f335153769e524354b4bb51a
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-18 18:48:34 +0000

    Added VaultProperty record type

---
+++ FluentVault/Domain/DataType.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentVault.Domain.Properties;
+
+public class DataType : BaseType
+{
+    public static readonly DataType Boolean = new("Bool");
+    public static readonly DataType DateTime = new("DateTime");
+    public static readonly DataType Image = new("Image");
+    public static readonly DataType Numeric = new("Numeric");
+    public static readonly DataType Text = new("String");
+
+    private DataType(string value) : base(value) { }
+
+    public static DataType Parse(string value)
+        => Parse(value, new[] { Boolean, DateTime, Image, Numeric, Text });
+}
---
+++ FluentVault/Domain/Properties/VaultProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentVault.Domain.Properties;
+
+public record VaultProperty(
+    long Id,
+    DataType DataType,
+    string DisplayName,
+    string SystemName,
+    bool IsActive,
+    bool IsUsedInBasicSearch,
+    bool IsSystemProperty,
+    long UsageCount);
