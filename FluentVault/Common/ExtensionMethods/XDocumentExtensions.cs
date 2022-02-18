513a2c27419c03afeb3e39758802fe1ad6175055
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-18 18:18:54 +0000

    Renamed some private methods

--- FluentVault/Common/ExtensionMethods/XDocumentExtensions.cs
+++ FluentVault/Common/ExtensionMethods/XDocumentExtensions.cs
@@ -5,31 +5,31 @@ namespace FluentVault;
 internal static class XDocumentExtensions
 {
     internal static string GetElementValue(this XContainer container, string name)
-        => container.GetElementByName(name).Value;
+        => container.GetSingleElementByName(name).Value;
 
     internal static string GetAttributeValue(this XElement element, string name)
         => element.Attribute(name)?.Value
         ?? throw new KeyNotFoundException($@"Attribute ""{name}"" was not found in element ""{element.Name}"".");
 
     internal static T ParseElementValue<T>(this XElement element, string name, Func<string, T> parse)
-        => parse(element.GetElementByName(name).Value);
+        => parse(element.GetSingleElementByName(name).Value);
 
     internal static T ParseAttributeValue<T>(this XElement element, string name, Func<string, T> parse)
         => parse(element.GetAttributeValue(name));
 
     internal static T ParseSingleElement<T>(this XContainer container, string name, Func<XElement, T> parse)
-        => parse(container.GetElementByName(name));
+        => parse(container.GetSingleElementByName(name));
 
     internal static IEnumerable<T> ParseAllElements<T>(this XContainer container, string name, Func<XElement, T> parse)
-        => container.GetElementsByName(name).Select(x => parse(x));
+        => container.GetAllElementsByName(name).Select(x => parse(x));
 
     internal static IEnumerable<T> ParseAllElementValues<T>(this XContainer container, string name, Func<string, T> parse)
-        => container.GetElementsByName(name).Select(x => parse(x.Value));
+        => container.GetAllElementsByName(name).Select(x => parse(x.Value));
 
-    private static XElement GetElementByName(this XContainer container, string name)
+    private static XElement GetSingleElementByName(this XContainer container, string name)
         => container.Descendants().FirstOrDefault(x => x.Name.LocalName.Equals(name))
         ?? throw new KeyNotFoundException($@"Element ""{name}"" was not found");
 
-    private static IEnumerable<XElement> GetElementsByName(this XContainer container, string name)
+    private static IEnumerable<XElement> GetAllElementsByName(this XContainer container, string name)
         => container.Descendants().Where(x => x.Name.LocalName.Equals(name));
 }
