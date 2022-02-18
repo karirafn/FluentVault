ef61e9e4b4d8ad299ead91fde53da3bc65eb2322
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-18 08:45:38 +0000

    Moved get lifecycles method to builder

--- FluentVault/VaultRequestBuilders/Get/GetRequestBuilder.cs
+++ FluentVault/VaultRequestBuilders/Get/GetRequestBuilder.cs
@@ -3,13 +3,22 @@
 internal class GetRequestBuilder : IGetRequestBuilder
 {
     private readonly VaultSessionInfo _session;
-    private readonly LifecycleService _lifecycleService;
 
     public GetRequestBuilder(VaultSessionInfo session)
     {
         _session = session;
-        _lifecycleService = new(_session);
     }
 
-    public async Task<IEnumerable<VaultLifecycle>> Lifecycles() => await _lifecycleService.GetAll();
+    public async Task<IEnumerable<VaultLifecycle>> Lifecycles()
+    {
+        var innerBody = @"<GetAllLifeCycleDefinitions xmlns=""http://AutodeskDM/Services/LifeCycle/1/7/2020/""/>";
+        var body = BodyBuilder.GetRequestBody(innerBody, _session.Ticket, _session.UserId);
+        Uri uri = new($"http://{_session.Server}/AutodeskDM/Services/v26/LifeCycleService.svc?op=GetAllLifeCycleDefinitions&uid=8&currentCommand=Connectivity.Explorer.Admin.AdminToolsCommand&vaultName={_session.Database}&app=VP");
+        var soapAction = @"""http://AutodeskDM/Services/LifeCycle/1/7/2020/LifeCycleService/GetAllLifeCycleDefinitions""";
+
+        var document = await VaultHttpClient.SendRequestAsync(uri, body, soapAction);
+        var lifecycles = document.ParseLifecycles();
+
+        return lifecycles;
+    }
 }
