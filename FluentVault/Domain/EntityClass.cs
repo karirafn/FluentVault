683ff7d2b6cae5c40e213c307f8533d1ce1dfe57
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-19 13:27:05 +0000

    Added CustomEntity as a EntityClass

--- FluentVault/Domain/EntityClass.cs
+++ FluentVault/Domain/EntityClass.cs
@@ -5,9 +5,10 @@ public class EntityClass : BaseType
     public static readonly EntityClass File = new("FILE");
     public static readonly EntityClass Folder = new("FLDR");
     public static readonly EntityClass Item = new("ITEM");
+    public static readonly EntityClass CustomEntity = new("CUSTENT");
 
     private EntityClass(string value) : base(value) { }
 
     public static EntityClass Parse(string value)
-        => Parse(value, new[] { File, Folder, Item });
+        => Parse(value, new[] { File, Folder, Item, CustomEntity });
 }
