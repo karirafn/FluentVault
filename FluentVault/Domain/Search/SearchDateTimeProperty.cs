4e1ae993a3380e28127ea29f9a9c69339a7edf20
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-19 17:05:00 +0000

    All system DateTime properties

---
+++ FluentVault/Domain/Search/SearchDateTimeProperty.cs
@@ -0,0 +1,17 @@
+﻿namespace FluentVault.Domain.Search;
+
+public enum SearchDateTimeProperty
+{
+    DateVersionCreated = 5,
+    CreateDate = 7,
+    CheckedIn = 8,
+    DateModified = 11,
+    CheckedOut = 17,
+    OriginalCreateDate = 25,
+    InitialReleaseDate = 40,
+    DueDate = 65,
+    DateSubmitted = 67,
+    DateCreated = 68,
+    Created = 73,
+    LatestReleasedDate = 240
+}
