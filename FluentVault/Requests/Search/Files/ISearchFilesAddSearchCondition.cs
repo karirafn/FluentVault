29f36668ee08faa2cc9d511bd34a80efefa85ef4
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-19 17:23:21 +0000

    Fixed interface name

--- FluentVault/Requests/Search/Files/ISearchFilesAddSearchCondition.cs
+++ FluentVault/Requests/Search/Files/ISearchFilesAddSearchCondition.cs
@@ -2,7 +2,7 @@
 
 public interface ISearchFilesAddSearchCondition
 {
-    public ISearchFilesRequestBuilder And { get; }
+    public ISearchFilesRequest And { get; }
     public Task<IEnumerable<VaultFile>> SearchAllAsync();
     public Task<VaultFile?> SearchSingleAsync();
 }
