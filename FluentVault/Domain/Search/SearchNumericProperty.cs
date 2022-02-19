bd227b472a0bdd8d0da31937644f5a79dcd581c5
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-19 17:10:33 +0000

    All system numeric properties

---
+++ FluentVault/Domain/Search/SearchNumericProperty.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentVault.Domain.Search;
+
+public enum SearchNumericProperty
+{
+    Version = 2,
+    NumberOfAttachments = 4,
+    FileSize = 12,
+    PropertyCompliance = 36,
+    PropertyComplianceHistorical = 37,
+    CategoryGlyph = 45,
+    CategoryGlyphHistorical = 46,
+    NumberOfFileAttachments = 69,
+    NumberOfItems = 70,
+    FileLinkState = 76,
+}
