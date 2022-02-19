6b3b98ccf5e7363c663190f7545e230d59c9d2e3
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-19 17:07:56 +0000

    All system Boolean properties

---
+++ FluentVault/Domain/Search/SearchBooleanProperties.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentVault.Domain.Search;
+
+public enum SearchBooleanProperties
+{
+    LinkedToItem = 13,
+    ItemAssignable = 14,
+    Hidden = 19,
+    LatestVersion = 20,
+    ControlledByChangeOrder = 21,
+    HasDrawing = 34,
+    HasParentRelationship = 35,
+    LatestReleasedRevision = 38,
+    ReleasedRevision = 39,
+    Obsolete = 42,
+    FileReplicated = 56,
+    HasModelState = 237,
+    IsTableDriven = 238,
+    IsTrueModelState = 239,
+}
