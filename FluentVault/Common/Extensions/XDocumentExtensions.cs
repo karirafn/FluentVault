919e0c71ceb7217bdc88f46070d2ed994d2509cc
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-20 12:35:14 +0000

    Simplified

--- FluentVault/Common/Extensions/XDocumentExtensions.cs
+++ FluentVault/Common/Extensions/XDocumentExtensions.cs
@@ -7,12 +7,8 @@ internal static class XDocumentExtensions
     internal static string GetElementValue(this XContainer container, string name)
         => container.GetSingleElementByName(name).Value;
 
-    internal static string GetAttributeValue(this XElement element, string name)
-        => element.Attribute(name)?.Value
-        ?? throw new KeyNotFoundException($@"Attribute ""{name}"" was not found in element ""{element.Name}"".");
-
     internal static T ParseElementValue<T>(this XElement element, string name, Func<string, T> parse)
-        => parse(element.GetSingleElementByName(name).Value);
+        => parse(element.GetElementValue(name));
 
     internal static T ParseAttributeValue<T>(this XElement element, string name, Func<string, T> parse)
         => parse(element.GetAttributeValue(name));
@@ -25,6 +21,10 @@ internal static class XDocumentExtensions
 
     internal static IEnumerable<T> ParseAllElementValues<T>(this XContainer container, string name, Func<string, T> parse)
         => container.GetAllElementsByName(name).Select(x => parse(x.Value));
+
+    internal static string GetAttributeValue(this XElement element, string name)
+        => element.Attribute(name)?.Value
+        ?? throw new KeyNotFoundException($@"Attribute ""{name}"" was not found in element ""{element.Name}"".");
 
     private static XElement GetSingleElementByName(this XContainer container, string name)
         => container.Descendants().FirstOrDefault(x => x.Name.LocalName.Equals(name))
