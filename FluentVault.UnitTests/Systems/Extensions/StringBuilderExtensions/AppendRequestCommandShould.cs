0bdc3c2fb091d9b1411811759820eb72ec6e32e9
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-27 19:19:24 +0000

    Added tests for AppendRequestCommand

---
+++ FluentVault.UnitTests/Systems/Extensions/StringBuilderExtensions/AppendRequestCommandShould.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+using FluentAssertions;
+
+using FluentVault.Common.Extensions;
+
+using Xunit;
+
+namespace FluentVault.UnitTests.Systems.Extensions.StringBuilderExtensions;
+
+public class AppendRequestCommandShould
+{
+    [Fact]
+    public void AppeendNothing_WhenCommandIsEmpty()
+    {
+        // Arrange
+        string name = "Name";
+        string command = string.Empty;
+        StringBuilder builder = new();
+
+        // Act
+        builder.AppendRequestCommand(name, command);
+
+        // Assert
+        builder.ToString().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void AppeendCommand_WhenCommandIsValid()
+    {
+        // Arrange
+        string name = "Name";
+        string command = "Command";
+        StringBuilder builder = new();
+        string expectation = "?op=Name&currentCommand=Command";
+
+        // Act
+        builder.AppendRequestCommand(name, command);
+
+        // Assert
+        builder.ToString().Should().Be(expectation);
+    }
+}
