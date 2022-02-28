e2e4dda3c7485c226979866ef37f032d7e1ab268
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-28 19:28:44 +0000

    Added AutoLogin property

--- FluentVault/Domain/VaultOptions.cs
+++ FluentVault/Domain/VaultOptions.cs
@@ -2,18 +2,11 @@
 
 public class VaultOptions
 {
+    public VaultOptions() { }
+
     public string Server { get; set; } = string.Empty;
     public string Database { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-
-    public VaultOptions(string server, string database, string username, string password)
-    {
-        Server = server;
-        Database = database;
-        Username = username;
-        Password = password;
-    }
-
-    public VaultOptions() { }
+    public bool AutoLogin { get; set; } = true;
 }
