c29c0757fd9897ee44e51a9f01c5fad6e76cbb69
Kári Rafn Karlsson <karirafn@gmail.com>
2022-02-28 08:44:03 +0000

    Re-ordered service registration

--- FluentVault/Configuration/ConfigureServices.cs
+++ FluentVault/Configuration/ConfigureServices.cs
@@ -3,19 +3,22 @@ using FluentVault.Domain.SOAP;
 
 using MediatR;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace FluentVault.Configuration;
 
 public static class ConfigureServices
 {
-    public static IServiceCollection AddFluentVault(this IServiceCollection services, IOptions<VaultOptions> options)
-        =>  services.AddMediatR(typeof(VaultClient).Assembly)
-            .AddSingleton<ISoapRequestService, SoapRequestService>()
+    public static IServiceCollection AddFluentVault(this IServiceCollection services, IConfiguration configuration)
+        =>  services
+            .AddOptions<VaultOptions>("Vault")
+            .Services
             .AddHttpClient("Vault", httpClient =>
                 {
-                    httpClient.BaseAddress = new Uri($@"http://{options.Value.Server}/");
+                    httpClient.BaseAddress = new Uri($@"http://{configuration.GetSection("Vault:Server").Value}/");
                 }).Services
+            .AddMediatR(typeof(VaultClient).Assembly)
+            .AddSingleton<ISoapRequestService, SoapRequestService>()
             .AddTransient<IVaultClient, VaultClient>();
 }
