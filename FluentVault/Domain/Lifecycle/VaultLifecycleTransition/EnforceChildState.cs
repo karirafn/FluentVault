bf5f4a3dc23fcf0d84247ba25c20c39f8bb0c49f
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-06 12:40:59 +0000

    Fixed smartenum names

--- FluentVault/Domain/Lifecycle/VaultLifecycleTransition/EnforceChildState.cs
+++ FluentVault/Domain/Lifecycle/VaultLifecycleTransition/EnforceChildState.cs
@@ -5,10 +5,10 @@ namespace FluentVault;
 public sealed class EnforceChildState : SmartEnum<EnforceChildState>
 {
     public static readonly EnforceChildState EnforceChildFiles = new(nameof(EnforceChildFiles), 1);
-    public static readonly EnforceChildState EnforceChildFolders = new(nameof(EnforceChildFiles), 2);
-    public static readonly EnforceChildState EnforceChildItems = new(nameof(EnforceChildFiles), 3);
-    public static readonly EnforceChildState EnforceChildItemsHaveBeenReleased = new(nameof(EnforceChildFiles), 4);
-    public static readonly EnforceChildState None = new(nameof(EnforceChildFiles), 5);
+    public static readonly EnforceChildState EnforceChildFolders = new(nameof(EnforceChildFolders), 2);
+    public static readonly EnforceChildState EnforceChildItems = new(nameof(EnforceChildItems), 3);
+    public static readonly EnforceChildState EnforceChildItemsHaveBeenReleased = new(nameof(EnforceChildItemsHaveBeenReleased), 4);
+    public static readonly EnforceChildState None = new(nameof(None), 5);
 
     private EnforceChildState(string name, int value) : base(name, value) { }
 }
