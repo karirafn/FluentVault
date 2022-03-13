9e871d51f5070f6a02b7bb97886ea0bdc10cce7c
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-13 14:36:59 +0000

    Added concrete folder id class

---
+++ FluentVault/Domain/Folder/FolderId.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Xml.Linq;
+
+using FluentVault.Common;
+using FluentVault.Extensions;
+
+namespace FluentVault.Domain.Folder;
+public class FolderId : GenericId<long>
+{
+    public FolderId(long value) : base(value) { }
+
+    public static FolderId ParseFromAttribute(XElement element, string key)
+        => new(long.TryParse(element.GetAttributeValue(key), out long value)
+            ? value
+            : throw new KeyNotFoundException("Failed to parse folder ID."));
+}
