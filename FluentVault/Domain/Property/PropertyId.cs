02b417597a1b92dd4f4ffb212b0424f954745da3
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-13 16:10:16 +0000

    Added concrete property id class

---
+++ FluentVault/Domain/Property/PropertyId.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+using FluentVault.Common;
+using FluentVault.Extensions;
+
+namespace FluentVault;
+public class PropertyId : GenericId<long>, IEquatable<PropertyId>, IComparable<PropertyId>
+{
+    public PropertyId(long value) : base(value) { }
+
+    public static PropertyId ParseFromAttribute(XElement element, string key)
+        => new(long.TryParse(element.GetAttributeValue(key), out long value)
+            ? value
+            : throw new KeyNotFoundException("Failed to parse property ID."));
+
+    public int CompareTo(PropertyId? other) => Value.CompareTo(other?.Value);
+    public bool Equals(PropertyId? other) => Value.Equals(other?.Value);
+    public override bool Equals(object? obj) => obj is PropertyId id && Equals(id);
+    public override int GetHashCode() => Value.GetHashCode();
+}
