854378bb1f6e66ce7a779d6df12f82320d06586e
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-13 16:09:52 +0000

    Added concrete master id class

---
+++ FluentVault/Domain/Common/MasterId.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Xml.Linq;
+
+using FluentVault.Common;
+using FluentVault.Extensions;
+
+namespace FluentVault;
+public class MasterId : GenericId<long>
+{
+    public MasterId(long value) : base(value) { }
+
+    public static MasterId ParseFromAttribute(XElement element, string key)
+        => new(long.TryParse(element.GetAttributeValue(key), out long value)
+            ? value
+            : throw new KeyNotFoundException("Failed to parse master ID."));
+}
