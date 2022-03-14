c2e8432a60e24f9f566d0d90906380aa81da1459
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-14 09:02:40 +0000

    Removed IConfiguration dependency from AddFluentVault

--- FluentVault/Configuration/ConfigureServices.cs
+++ FluentVault/Configuration/ConfigureServices.cs
@@ -10,14 +10,19 @@ namespace FluentVault.Configuration;
 
 public static class ConfigureServices
 {
-    public static IServiceCollection AddFluentVault(this IServiceCollection services, IConfiguration configuration)
-        =>  services
-            .Configure<VaultOptions>(configuration.GetSection(nameof(VaultOptions)))
-            .AddHttpClient("Vault", httpClient =>
-                {
-                    httpClient.BaseAddress = new Uri($@"http://{configuration.GetSection($"{nameof(VaultOptions)}:Server").Value}/");
-                }).Services
-            .AddMediatR(typeof(VaultClient).Assembly)
-            .AddSingleton<IVaultRequestService, VaultRequestService>()
-            .AddTransient<IVaultClient, VaultClient>();
+    public static IServiceCollection AddFluentVault(this IServiceCollection services)
+    {
+        IConfiguration configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+        string server = configuration.GetSection($"{nameof(VaultOptions)}:Server").Value;
+
+        return services
+                   .Configure<VaultOptions>(configuration.GetSection(nameof(VaultOptions)))
+                   .AddHttpClient("Vault", httpClient =>
+                       {
+                           httpClient.BaseAddress = new Uri($@"http://{server}/");
+                       }).Services
+                   .AddMediatR(typeof(VaultClient).Assembly)
+                   .AddSingleton<IVaultRequestService, VaultRequestService>()
+                   .AddTransient<IVaultClient, VaultClient>();
+    }
 }
