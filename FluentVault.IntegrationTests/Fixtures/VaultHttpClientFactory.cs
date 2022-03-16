48e9cb3913330a73b0db417380bfa5e80186a5c5
Kári Rafn Karlsson <karirafn@gmail.com>
2022-03-16 18:31:49 +0000

    Removed hard coded vault server

--- FluentVault.IntegrationTests/Fixtures/VaultHttpClientFactory.cs
+++ FluentVault.IntegrationTests/Fixtures/VaultHttpClientFactory.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 using System.Net.Http;
 
+using Microsoft.Extensions.Configuration;
+
 namespace FluentVault.IntegrationTests.Fixtures;
 public class VaultHttpClientFactory : IHttpClientFactory
 {
+    private readonly string _server;
+
+    public VaultHttpClientFactory()
+    {
+        _server = new ConfigurationBuilder()
+            .AddUserSecrets<VaultOptionsFixture>()
+            .Build()
+            .GetValue<string>(nameof(VaultOptions.Server));
+    }
+
     public HttpClient CreateClient(string name)
-        => new() { BaseAddress = new Uri(@"http://ska-vaultpro/") };
+        => new() { BaseAddress = new Uri($@"http://{_server}/") };
 }
