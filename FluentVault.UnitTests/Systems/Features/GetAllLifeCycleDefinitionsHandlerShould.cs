714db01728a23458f5ff3bf0bd9420e06f68b9fb
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-10 08:32:36 +0000

    Added get all life cycle definitions handler test

---
+++ FluentVault.UnitTests/Systems/Features/GetAllLifeCycleDefinitionsHandlerShould.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using FluentAssertions;
+
+using FluentVault.Common;
+using FluentVault.Features;
+using FluentVault.TestFixtures.LifeCycle;
+using FluentVault.UnitTests.Helpers;
+
+using Moq;
+
+using Xunit;
+
+namespace FluentVault.UnitTests.Systems.Features;
+public class GetAllLifeCycleDefinitionsHandlerShould
+{
+    private static readonly VaultLifeCycleDefinitionFixture _fixture = new();
+
+    [Fact]
+    public async Task ValVaultService()
+    {
+        // Arrange
+        int count = 5;
+        IEnumerable<VaultLifeCycleDefinition> expectation = _fixture.CreateMany(count);
+        XDocument response = _fixture.ParseXDocument(expectation);
+        Mock<IVaultService> vaultService = new();
+
+        vaultService.Setup(VaultServiceExpressions.SendAsync)
+            .ReturnsAsync(response);
+
+        GetAllLifeCycleDefinitionsQuery query = new();
+        GetAllLifeCycleDefinitionsHandler sut = new(vaultService.Object);
+
+        // Act
+        IEnumerable<VaultLifeCycleDefinition> result = await sut.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectation);
+        vaultService.Verify(VaultServiceExpressions.SendAsync, Times.Once());
+    }
+}
