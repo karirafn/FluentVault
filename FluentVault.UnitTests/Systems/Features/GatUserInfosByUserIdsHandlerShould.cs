96021036e1ed5594d0065df9d507d1ceb33876a2
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-10 08:46:31 +0000

    Added get user infor by user ids handler test

---
+++ FluentVault.UnitTests/Systems/Features/GatUserInfosByUserIdsHandlerShould.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using FluentAssertions;
+
+using FluentVault.Common;
+using FluentVault.Features;
+using FluentVault.TestFixtures.User;
+using FluentVault.UnitTests.Helpers;
+
+using Moq;
+
+using Xunit;
+
+namespace FluentVault.UnitTests.Systems.Features;
+public class GatUserInfosByUserIdsHandlerShould
+{
+    private static readonly VaultUserInfoFixture _fixture = new();
+
+    [Fact]
+    public async Task ValVaultService()
+    {
+        // Arrange
+        VaultUserInfo expectation = _fixture.Create();
+        XDocument response = _fixture.ParseXDocument(expectation);
+        Mock<IVaultService> vaultService = new();
+
+        vaultService.Setup(VaultServiceExpressions.SendAsync)
+            .ReturnsAsync(response);
+
+        GetUserInfosByIserIdsQuery query = new(new[] { expectation.User.Id });
+        GetUserInfosByUserIdsHandler sut = new(vaultService.Object);
+
+        // Act
+        IEnumerable<VaultUserInfo> result = await sut.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Single().Should().BeEquivalentTo(expectation);
+        vaultService.Verify(VaultServiceExpressions.SendAsync, Times.Once());
+    }
+}
