a55e7a546fd562590cc167a3cbb8442cbae6d62f
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-10 08:38:39 +0000

    Added get all categories handler test

---
+++ FluentVault.UnitTests/Systems/Features/GetAllCategoriesHandlerShould.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using FluentAssertions;
+
+using FluentVault.Common;
+using FluentVault.Features;
+using FluentVault.TestFixtures.Category;
+using FluentVault.UnitTests.Helpers;
+
+using Moq;
+
+using Xunit;
+
+namespace FluentVault.UnitTests.Systems.Features;
+public class GetAllCategoryConfigurations
+{
+    private static readonly VaultCategoryFixture _fixture = new();
+
+    [Fact]
+    public async Task ValVaultService()
+    {
+        // Arrange
+        int count = 5;
+        IEnumerable<VaultCategory> expectation = _fixture.CreateMany(count);
+        XDocument response = _fixture.ParseXDocument(expectation);
+        Mock<IVaultService> vaultService = new();
+
+        vaultService.Setup(VaultServiceExpressions.SendAsync)
+            .ReturnsAsync(response);
+
+        GetAllCategoryConfigurationsQuery query = new();
+        GetAllCategoryConfigurationsHandler sut = new(vaultService.Object);
+
+        // Act
+        IEnumerable<VaultCategory> result = await sut.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectation);
+        vaultService.Verify(VaultServiceExpressions.SendAsync, Times.Once());
+    }
+}
