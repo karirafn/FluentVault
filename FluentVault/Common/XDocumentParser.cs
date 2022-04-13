45adf23934f90a7eb1116110b8687bd39e034b20
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-13 08:54:28 +0000

    Added abstract XDocument and XElement parsing classes

---
+++ FluentVault/Common/XDocumentParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+using FluentVault.Extensions;
+
+namespace FluentVault.Common;
+public abstract class XDocumentParser<T> : XElementParser<T>
+{
+    private readonly string _elementName;
+
+    public XDocumentParser(string elementName)
+    {
+        _elementName = elementName;
+    }
+
+    internal T ParseSingle(XDocument document)
+        => document.ParseElement(_elementName, Parse);
+
+    internal IEnumerable<T> ParseAll(XDocument document)
+        => document.ParseAllElements(_elementName, Parse);
+}
---
+++ FluentVault/Common/XElementParser.cs
@@ -0,0 +1,7 @@
+﻿using System.Xml.Linq;
+
+namespace FluentVault.Common;
+public abstract class XElementParser<T>
+{
+    protected internal abstract T Parse(XElement element);
+}
