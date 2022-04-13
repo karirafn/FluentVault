ae584894a42b922cc3e3d170e4af0553f1e35a81
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-13 11:17:59 +0000

    Added abstract XDocument and XElement serializers

---
+++ FluentVault/Common/XDocumentSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+using FluentVault.Extensions;
+
+namespace FluentVault.Common;
+internal abstract class XDocumentSerializer<T> : XElementSerializer<T>
+{
+    private readonly string _name;
+
+    public XDocumentSerializer(string name, XNamespace @namespace) : base(@namespace)
+    {
+        _name = name;
+    }
+
+    public T DeserializeSingle(XDocument document)
+        => document.ParseElement(_name, Deserialize);
+
+    public IEnumerable<T> DeserializeAll(XDocument document)
+        => document.ParseAllElements(_name, Deserialize);
+}
---
+++ FluentVault/Common/XElementSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+namespace FluentVault.Common;
+internal abstract class XElementSerializer<T>
+{
+    protected XNamespace Namespace { get; }
+
+    public XElementSerializer(XNamespace @namespace)
+    {
+        Namespace = @namespace;
+    }
+
+    internal abstract XElement Serialize(T entity);
+    internal abstract T Deserialize(XElement element);
+}
