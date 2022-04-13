a5aa43e5d3d453d7674c241621bbf2a0941b7a62
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-13 12:15:07 +0000

    Added serializer

--- FluentVault/Domain/Search/SearchStatus.cs
+++ FluentVault/Domain/Search/SearchStatus.cs
@@ -1,11 +1,22 @@
 ﻿using System.Xml.Linq;
 
+using FluentVault.Common;
 using FluentVault.Extensions;
 
 namespace FluentVault.Domain.Search;
-public record SearchStatus(int TotalHits, IndexingStatus IndexingStatus)
+public record SearchStatus(int TotalHits, IndexingStatus IndexingStatus);
+
+internal class SearchStatusSerializer : XElementSerializer<SearchStatus>
 {
-    internal static SearchStatus Parse(XElement element)
-        => new(element.ParseAttributeValue(nameof(TotalHits), int.Parse),
+    public SearchStatusSerializer(XNamespace @namespace) : base("searchstatus", @namespace)
+    {
+    }
+
+    internal override SearchStatus Deserialize(XElement element)
+        => new(element.ParseAttributeValue(nameof(SearchStatus.TotalHits), int.Parse),
             element.ParseAttributeValue("IndxStatus", x => IndexingStatus.FromName(x)));
+
+    internal override XElement Serialize(SearchStatus entity)
+        => BaseElement.AddAttribute(nameof(SearchStatus.TotalHits), entity.TotalHits)
+            .AddAttribute("IndxStatus", entity.IndexingStatus);
 }
