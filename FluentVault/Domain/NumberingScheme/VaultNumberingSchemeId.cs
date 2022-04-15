0c4b2353a595fdd1a44951ffd1e699fb08bf9875
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-15 21:34:43 +0000

    Added numbering scheme id

---
+++ FluentVault/Domain/NumberingScheme/VaultNumberingSchemeId.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentVault.Common;
+
+namespace FluentVault;
+public class VaultNumberingSchemeId : VaultGenericId<long>
+{
+    public VaultNumberingSchemeId(long value) : base(value) { }
+
+    public static VaultFileId Parse(string value)
+        => new(long.TryParse(value, out long id)
+            ? id
+            : throw new KeyNotFoundException("Failed to parse numbering scheme ID."));
+}
