00962bd7fbe8a67fb1c540523da00efd9fb3555e
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-15 21:34:11 +0000

    Added file iteration id

---
+++ FluentVault/Domain/FileIteration/VaultFileIterationId.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentVault.Common;
+
+namespace FluentVault;
+public class VaultFileIterationId : VaultGenericId<long>
+{
+    public VaultFileIterationId(long value) : base(value) { }
+
+    public static VaultFileId Parse(string value)
+        => new(long.TryParse(value, out long id)
+            ? id
+            : throw new KeyNotFoundException("Failed to parse file iteration ID."));
+}
