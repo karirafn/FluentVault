04fe42fba2e5ff2741a470da9aa40c73d1ae7c18
Kári Rafn Karlsson <karirafn@gmail.com>
2022-04-18 11:08:33 +0000

    Now uses VaultOptionsFixture to get server name

--- FluentVault.IntegrationTests/Fixtures/VaultHttpClientFactory.cs
+++ FluentVault.IntegrationTests/Fixtures/VaultHttpClientFactory.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Net.Http;
 
-using Microsoft.Extensions.Configuration;
-
 namespace FluentVault.IntegrationTests.Fixtures;
 public class VaultHttpClientFactory : IHttpClientFactory
 {
     private readonly string _server;
 
     public VaultHttpClientFactory()
-        => _server = new ConfigurationBuilder()
-            .AddUserSecrets<VaultOptionsFixture>()
-            .Build()
-            .GetValue<string>(nameof(VaultOptions.Server));
+    {
+        _server = new VaultOptionsFixture().Create().Value.Server;
+    }
 
     public HttpClient CreateClient(string name)
         => new() { BaseAddress = new Uri($@"http://{_server}/") };
