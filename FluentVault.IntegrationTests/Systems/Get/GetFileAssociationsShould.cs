69655b24ccf444a8f55e0d4d0c3fd3bf27a8aecd
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-13 15:14:09 +0000

    Addedget file associations test

---
+++ FluentVault.IntegrationTests/Systems/Get/GetFileAssociationsShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using FluentVault.IntegrationTests.Fixtures;
+
+using Xunit;
+
+namespace FluentVault.IntegrationTests.Systems.Get;
+public class GetFileAssociationsShould
+{
+    private static readonly VaultTestData _testData = new();
+
+    [Fact]
+    public async Task ReturnEmptyArray_WhenFileHasNoAssociations()
+    {
+        // Arrange
+        VaultServiceProvider provider = new();
+        IVaultClient sut = provider.GetRequiredService<IVaultClient>();
+
+        // Act
+        IEnumerable<VaultFileAssociation> result = await sut.Get.File.Associations
+            .ByFileIterationId(_testData.TestPartIterationId)
+            .ExecuteAsync(CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+}
