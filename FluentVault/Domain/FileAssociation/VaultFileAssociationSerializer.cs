2f984959bbe1b85dcf3e4a02fd0beedf6e77ed0c
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-13 15:03:41 +0000

    Added file association models and serializer

---
+++ FluentVault/Domain/FileAssociation/VaultFileAssociationSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Linq;
+
+using FluentVault.Common;
+using FluentVault.Extensions;
+
+namespace FluentVault;
+internal class VaultFileAssociationSerializer : XElementSerializer<VaultFileAssociation>
+{
+    private const string FileAssoc = nameof(FileAssoc);
+    private const string Typ = nameof(Typ);
+    private const string RefId = nameof(RefId);
+    private const string VaultPathChanged = nameof(VaultPathChanged);
+    private const string CorrectRev = nameof(CorrectRev);
+    private const string ParFile = nameof(ParFile);
+    private const string CldFile = nameof(CldFile);
+
+    private readonly VaultFileSerializer _parentFileSerializer;
+    private readonly VaultFileSerializer _childFileSerializer;
+
+    public VaultFileAssociationSerializer(XNamespace @namespace) : base(FileAssoc, @namespace) 
+    {
+        _parentFileSerializer = new(ParFile, @namespace);
+        _childFileSerializer = new(CldFile, @namespace);
+    }
+
+    internal override XElement Serialize(VaultFileAssociation association) =>
+        BaseElement
+            .AddAttribute(nameof(VaultFileAssociation.Id), association.Id)
+            .AddAttribute(Typ, association.Type)
+            .AddAttribute(nameof(VaultFileAssociation.Source), association.Source)
+            .AddAttribute(RefId, association.ReferenceId)
+            .AddAttribute(nameof(VaultFileAssociation.ExpectedVaultPath), association.ExpectedVaultPath)
+            .AddAttribute(VaultPathChanged, association.HasVaultPathChanged)
+            .AddAttribute(CorrectRev, association.IsCorrectRevision)
+            .AddElement(_parentFileSerializer.Serialize(association.Parent))
+            .AddElement(_childFileSerializer.Serialize(association.Parent));
+
+    internal override VaultFileAssociation Deserialize(XElement element)
+        => new(element.ParseAttributeValue(nameof(VaultFileAssociation.Id), VaultFileAssociationId.Parse),
+            element.ParseAttributeValue(Typ, x => VaultFileAssociationType.FromName(x)),
+            element.GetAttributeValue(nameof(VaultFileAssociation.Source)),
+            element.GetAttributeValue(RefId),
+            element.GetAttributeValue(nameof(VaultFileAssociation.ExpectedVaultPath)),
+            element.ParseAttributeValue(VaultPathChanged, bool.Parse),
+            element.ParseAttributeValue(CorrectRev, bool.Parse),
+            _parentFileSerializer.Deserialize(element),
+            _childFileSerializer.Deserialize(element));
+}
---
+++ FluentVault/Domain/FileAssociation/VaultFileAssociationType.cs
@@ -0,0 +1,13 @@
+﻿
+using Ardalis.SmartEnum;
+
+namespace FluentVault;
+public sealed class VaultFileAssociationType : SmartEnum<VaultFileAssociationType>
+{
+    public static readonly VaultFileAssociationType Attachment = new(nameof(Attachment), 1);
+    public static readonly VaultFileAssociationType Dependency = new(nameof(Dependency), 2);
+    public static readonly VaultFileAssociationType All = new(nameof(All), 3);
+    public static readonly VaultFileAssociationType None = new(nameof(None), 4);
+
+    private VaultFileAssociationType(string name, int value) : base(name, value) { }
+}
