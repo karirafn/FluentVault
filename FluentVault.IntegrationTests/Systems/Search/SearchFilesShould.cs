59594bec4ac0ddfc9854a7e5871d2a38d22f3061
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-13 13:05:39 +0000

    Moved tests

---
+++ FluentVault.IntegrationTests/Systems/Search/SearchFilesShould.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using FluentVault.IntegrationTests.Fixtures;
+
+using Xunit;
+
+namespace FluentVault.IntegrationTests.Systems.Search;
+public class SearchFilesShould
+{
+    private static readonly VaultTestData _testData = new();
+
+    // This test will fail if the Vault has no part files
+    [Fact]
+    public async Task FindFiles()
+    {
+        // Arrange
+        VaultServiceProvider provider = new();
+        IVaultClient sut = provider.GetRequiredService<IVaultClient>();
+
+        // Act
+        IEnumerable<VaultFile> result = await sut.Search.Files
+            .BySystemProperty(VaultSearchProperty.FileExtension)
+            .Containing("ipt")
+            .GetPagedResultAsync();
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+    }
+
+    // This test will fail if test part has fewer than 2 versions
+    [Fact]
+    public async Task FindMultipleVersions_WhenCallingCetAllVersions()
+    {
+        // Arrange
+        VaultServiceProvider provider = new();
+        IVaultClient sut = provider.GetRequiredService<IVaultClient>();
+
+        // Act
+        IEnumerable<VaultFile> result = await sut.Search.Files
+            .BySystemProperty(VaultSearchProperty.FileName)
+            .EqualTo(_testData.TestPartFilename)
+            .AllVersions
+            .GetPagedResultAsync();
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+        result.Should().HaveCountGreaterThan(1);
+        result.Should().AllSatisfy(file => file.MasterId.Value.Should().Be(_testData.TestPartMasterId));
+    }
+
+    [Fact]
+    public async Task FindSingleFile()
+    {
+        // Arrange
+        VaultServiceProvider provider = new();
+        IVaultClient sut = provider.GetRequiredService<IVaultClient>();
+
+        // Act
+        VaultFile? result = await sut.Search.Files
+            .BySystemProperty(VaultSearchProperty.FileName)
+            .Containing(_testData.TestPartFilename)
+            .GetFirstResultAsync();
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+}
