24af67ca18f8c46b64bc30c2f3ae7cf71864aad7
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-14 17:52:19 +0000

    Added vault entity ID

---
+++ FluentVault/Domain/Entity/VaultEntityId.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentVault.Common;
+
+namespace FluentVault;
+public class VaultEntityId : VaultGenericId<long>
+{
+    public VaultEntityId(long value) : base(value) { }
+
+    public static VaultEntityId Parse(string value)
+        => new(long.TryParse(value, out long id)
+            ? id
+            : throw new KeyNotFoundException("Failed to parse entity ID."));
+}
