09d4fc57494fba76cc956dc54f3d900740092d8c
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-14 16:46:53 +0000

    Changed hardcored string to nameof

--- FluentVault/Domain/Common/VaultEntityClass.cs
+++ FluentVault/Domain/Common/VaultEntityClass.cs
@@ -4,10 +4,13 @@ namespace FluentVault;
 
 public sealed class VaultEntityClass : SmartEnum<VaultEntityClass>
 {
+    private const string FLDR = nameof(FLDR);
+    private const string CUSTENT = nameof(CUSTENT);
+
     public static readonly VaultEntityClass File = new(nameof(File).ToUpper(), 1);
-    public static readonly VaultEntityClass Folder = new("FLDR", 2);
+    public static readonly VaultEntityClass Folder = new(FLDR, 2);
     public static readonly VaultEntityClass Item = new(nameof(Item).ToUpper(), 3);
-    public static readonly VaultEntityClass CustomEntity = new("CUSTENT", 4);
+    public static readonly VaultEntityClass CustomEntity = new(CUSTENT, 4);
 
     private VaultEntityClass(string name, int value) : base(name, value) { }
 }
