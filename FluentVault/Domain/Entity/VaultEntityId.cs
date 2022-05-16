3d3b007d882bdfed03f82e3f642648e06a6bed58
Kári Rafn Karlsson <karirafn@gmail.com>
2022-05-16 10:13:32 +0000

    Added implicit conversion operators to file, item and folder ids

--- FluentVault/Domain/Entity/VaultEntityId.cs
+++ FluentVault/Domain/Entity/VaultEntityId.cs
@@ -6,6 +6,10 @@ public class VaultEntityId : VaultGenericId<long>
 {
     public VaultEntityId(long value) : base(value) { }
 
+    public static implicit operator VaultFileId(VaultEntityId entityId) => new(entityId.Value);
+    public static implicit operator VaultItemId(VaultEntityId entityId) => new(entityId.Value);
+    public static implicit operator VaultFolderId(VaultEntityId entityId) => new(entityId.Value);
+
     public static VaultEntityId Parse(string value)
         => new(long.TryParse(value, out long id)
             ? id
